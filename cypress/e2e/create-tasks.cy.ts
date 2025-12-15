32099ab1b4d216a93b9735e80d664ad62f4b9c19
Daniels <danixkanepe@gmail.com>
2025-12-15 12:25:41 +0100

    Replace add-task test with create-tasks test
    Removed the add-task.cy.ts test and added create-tasks.cy.ts with updated test cases for task title validation. The new tests focus on scenarios where the task title is empty or too short.

---
+++ cypress/e2e/create-tasks.cy.ts
@@ -0,0 +1,23 @@
+describe('TEST-1 Task title', () => {
+
+  it('Task title is empty', function() {
+    cy.visit('http://localhost:3000')
+    cy.get('#root input.w-full').click();
+    cy.get('#root button.text-white').click();
+    cy.get('#root p.text-slate-500.font-medium').should('have.text', 'No tasks found');
+    cy.get('#root p.text-slate-500.font-medium').should('be.visible');
+    cy.get('#root textarea.w-full').click();
+    cy.get('#root textarea.w-full').type('asdasd');
+    cy.get('#root button.text-white').click();
+    cy.get('#root p.text-slate-500.font-medium').should('be.visible');
+    cy.get('#root p.text-slate-500.font-medium').should('have.text', 'No tasks found');
+  });
+
+  it('Task title is is below 3 char', function() {
+    cy.visit('http://localhost:3000')
+  });
+
+});
+
+it('Task title not enough characters', function() {});
+
